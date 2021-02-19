58963b92a93054e5eedb2ee6d9f3d04d5957a5e4
LuisLazcanoOrtiz <73734150+LuisLazcanoOrtiz@users.noreply.github.com>
2021-02-19 02:06:45 -0800

    Update Control_jugador.cs

--- Assets/Scripts/Control_jugador.cs
+++ Assets/Scripts/Control_jugador.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Control_jugador : MonoBehaviour
+public class Problem1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Arreglo
+        int[] nums = {8,1,2,2,3};
+        int[] output = NumbersLessThan(nums);
+
+       //recorre cada numero del arreglo 
+       foreach (var num in output)
+       {
+            Debug.Log(num);//imprime el arreglo
+       }
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    private int[] NumbersLessThan(int[] nums)
     {
         
-    }
+        //Matrices auxiliares 
+        int[] aux= nums; 
+        int[] aux2 = {0,0,0,0,0};
+       
+        for ( int i = 0; i < nums.Length; i++) { 
+            
+            for (int j = 0; j < aux.Length; j++) 
+            {
+                if (aux[j] < nums[i]){
+                    aux2[i]++;
+                    }
+            }
+    
+        } 
+
+
+      return aux2;  
+    } 
+         
 }
